8057ab5cc67b304352b206f9676d63db188a8d0d
Spencer Carli <spencercarli@gmail.com>
2016-06-28 20:19:41 -0400

    Functioning camera

--- app/routes/Camera.js
+++ app/routes/Camera.js
@@ -1,21 +1,109 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableHighlight,
+  Image,
+  Text,
+} from 'react-native';
+import Camera from 'react-native-camera';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#2C4770',
+    backgroundColor: '#000',
   },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    borderWidth: 5,
+    borderColor: '#FFF',
+    marginBottom: 15,
+  },
+  cancel: {
+    position: 'absolute',
+    right: 20,
+    top: 20,
+    backgroundColor: 'transparent',
+    color: '#FFF',
+    fontWeight: '600',
+    fontSize: 17,
+  }
 });
 
-const Camera = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Camera</Text>
-    </View>
-  );
+class CameraRoute extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      path: null,
+    };
+  }
+
+  takePicture() {
+    this.camera.capture()
+      .then((data) => {
+        console.log(data);
+        this.setState({ path: data.path })
+      })
+      .catch(err => console.error(err));
+  }
+
+  renderCamera() {
+    return (
+      <Camera
+        ref={(cam) => {
+          this.camera = cam;
+        }}
+        style={styles.preview}
+        aspect={Camera.constants.Aspect.fill}
+        captureTarget={Camera.constants.CaptureTarget.disk}
+      >
+        <TouchableHighlight
+          style={styles.capture}
+          onPress={this.takePicture.bind(this)}
+          underlayColor="rgba(255, 255, 255, 0.5)"
+        >
+          <View />
+        </TouchableHighlight>
+      </Camera>
+    );
+  }
+
+  renderImage() {
+    return (
+      <View>
+        <Image
+          source={{ uri: this.state.path }}
+          style={styles.preview}
+        />
+        <Text
+          style={styles.cancel}
+          onPress={() => this.setState({ path: null })}
+        >Cancel
+        </Text>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.path ? this.renderImage() : this.renderCamera()}
+      </View>
+    );
+  }
 };
 
-export default Camera;
+export default CameraRoute;
